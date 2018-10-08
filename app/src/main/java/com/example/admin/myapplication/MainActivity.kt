2517d0ee273b788d10f7b21c825e95462e00d621
dohieu19999@gmail.com <Dobatrang03031966>
2018-10-08 22:25:12 +0700

    happy

---
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.admin.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+//        var mangso: IntArray= intArrayOf(1,2,34,5,6,6)
+//        mangso.get(2)
+//        print(mangso.get(0))
+//        Log.d("BBB",mangso.get(0).toString())
+
+
+
+    }
+}
